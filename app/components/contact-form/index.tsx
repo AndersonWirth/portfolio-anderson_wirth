abdb0562d889035e213924b21d20993271823a45
Anderson Wirth <andersonwirth1@gmail.com>
2025-04-05 10:41:37 -0300

    Commit: Footer e mais estilizações criadas.

---
+++ app/components/contact-form/index.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { z } from "zod";
+import { Button } from "../button";
+import { SectionTitle } from "../section-title";
+
+const contactFormSchema = z.object({
+    name: z.string().min(3).max(100),
+    email: z.string().email(),
+    message: z.string().min(1).max(500),
+})
+
+type ContactFormData = z.infer<typeof contactFormSchema>
+
+export const ContactForm = () => {
+
+    const { handleSubmit, register } = useForm({
+        resolver: zodResolver(contactFormSchema)
+    })
+
+    const onSubmit = (data: ContactFormData) => {
+        console.log(data)
+    }
+
+    return (
+        <section className="py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950">
+            <div className="w-full max-w-[420px] mx-auto">
+                <SectionTitle
+                    subtitle="contato"
+                    title="Vamor trabalhar juntos? Entre em contato"
+                    className="items-center text-center"
+                />
+
+                <form
+                    className="mt-12 w-full flex flex-col gap-4"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+
+                    <input
+                        {...register('name')}
+                        placeholder="None"
+                        className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-4 ring-emerald-400"
+                    />
+                    <input
+                        {...register('email')}
+                        placeholder="E-mail"
+                        className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-4 ring-emerald-400"
+                    />
+                    <textarea
+                        {...register('message')}
+                        placeholder="Mensagem..."
+                        className="resize-none w-full h-[138px] bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-4 ring-emerald-400"
+                        maxLength={500}
+                    />
+
+                    <Button className="mt-6 w-max mx-auto shadow-button">
+                        Enviar Mensagem
+                        <HiArrowNarrowRight size={18} />
+                    </Button>
+                </form>
+            </div>
+        </section >
+    )
+
+}
---
+++ app/components/footer/index.tsx
@@ -0,0 +1,15 @@
+
+import Link from 'next/link'
+import { IoMdHeart } from 'react-icons/io'
+
+export const Footer = () => {
+    return (
+        <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
+            <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
+                Made by
+                <IoMdHeart size={13} className="text-emerald-500" />
+                <Link target='_blank' href='https://www.linkedin.com/in/anderson-engel-wirth/' className='text-emerald-400 hover:text-emerald-200'>Anderson Wirth</Link>
+            </span>
+        </footer>
+    )
+}
